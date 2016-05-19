5604a160b67cb30a7e2ff507acf920bd63435483
SoeunLee <soeun2327@gmail.com>
2016-05-19 17:32:57 +0900

    test

--- ExplorerMain.java
+++ ExplorerMain.java
@@ -4,5 +4,6 @@ public class ExplorerMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DrawFrame explorer = new DrawFrame();
+		System.out.println("Hi");
 	}
 }
