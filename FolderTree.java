2a258246171d754401ef43153561ade4ad46771f
ppang <yucaroll@naver.com>
2016-05-25 01:58:37 +0900

    완성

--- FolderTree.java
+++ FolderTree.java
@@ -7,126 +7,104 @@ import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.event.TreeWillExpandListener;
-import javax.swing.filechooser.FileSystemView;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.ExpandVetoException;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
-public class FolderTree extends JTree implements TreeWillExpandListener, TreeSelectionListener{
-	private FileSystemView filesystem;
+public class FolderTree extends JTree implements TreeWillExpandListener, TreeSelectionListener {
+	private final String EMPTYTAG = "EMPTY";
 	private DefaultMutableTreeNode root;
-	
-	public FolderTree(){
+
+	public FolderTree() {
 		initTree();
 		initFileSystem();
-        /*
-        DefaultMutableTreeNode vegetableNode = new DefaultMutableTreeNode("Vegetables");
-        DefaultMutableTreeNode fruitNode = new DefaultMutableTreeNode("Fruits");
- 
-        vegetableNode.add(new DefaultMutableTreeNode("Capsicum"));
-        vegetableNode.add(new DefaultMutableTreeNode("Carrot"));
-        vegetableNode.add(new DefaultMutableTreeNode("Tomato"));
-        vegetableNode.add(new DefaultMutableTreeNode("Potato"));
-        
-        //add the child nodes to the root node
-        root.add(vegetableNode);
-        root.add(fruitNode);
-        */
-        DefaultTreeModel model = new DefaultTreeModel(root);
-        setModel(model);
-        
+		
+		DefaultTreeModel model = new DefaultTreeModel(root);
+		setModel(model);
+
 	}
-	
-	private void initTree(){
+
+	private void initTree() {
 		addTreeWillExpandListener(this);
 
-		root = new DefaultMutableTreeNode();
+		root = new DefaultMutableTreeNode("root");
 	}
-	
-	private void initFileSystem(){
+
+	private void initFileSystem() {
 		File[] dir = File.listRoots();
-		for(int i=0; i<dir.length; i++){
+		for (int i = 0; i < dir.length; i++) {
 			DefaultMutableTreeNode node = new DefaultMutableTreeNode(dir[i].toString());
+			node.add(new DefaultMutableTreeNode(EMPTYTAG));
 			root.add(node);
 		}
+	}
+
+	private String getPath(TreePath tp) {
+		/**
+		 * TreePath로 부터 String형의 파일경로를 만들어 반환한다.
+		 * @param (TreePath) [root, /, .fseventsd] 같은 형태로 들어오면 
+		 * @return (String) /.fseventsd/ 이렇게 바꿔줌 
+		 */
 		
-		/*
-		File[] f = filesystem.getRoots();
-		root.setUserObject(f[0]);
-		
-		for(int i=0; i<f.length; i++)
-			System.out.println(filesystem.getSystemDisplayName(f[i]));
-		
-		
-		
-		File[] list = getChildFolder(f[0]);
-		for(int i=0; i<list.length; i++){
-			DefaultMutableTreeNode test = new DefaultMutableTreeNode();
-			test.setUserObject(list[i]);
-			root.add(test);
-			//System.out.println(filesystem.getSystemDisplayName(list[i]));
+		String realPath = "";
+		String treePath = tp.toString();
+		String path = treePath.substring(1, treePath.length() - 1); // [ ] 없애기
+
+		String[] pathToken = path.split(", "); // , 단위로 분리 
+		for (int i = 1; i < pathToken.length; i++) { // 분리한 토큰을 separator '/'를 포함한 파일경로로 변환. 
+			realPath += pathToken[i];
+			if (i != 1)
+				realPath += File.separator;
 		}
-		*/
+
+		return realPath;
 	}
-	/*
-	private File[] getChildFolder(File f){
-		File[] files = f.listFiles();
-		ArrayList<File> fileArray = new ArrayList<File>();
-		for(int i=0; i<files.length; i++){
-			if(files[i].isDirectory())
-				fileArray.add(files[i]);
-		}
-		
-		File[] fileList = new File[fileArray.size()];
-		fileArray.toArray(fileList);
-		
-		return fileList;
-	}
-	 */
-	
-	private String getPos(TreePath tp){
-		StringTokenizer tk = new StringTokenizer(tp.toString(), "[,]");
-		String str = "";
-		tk.nextToken();
-		while(tk.hasMoreTokens())
-			str += tk.nextToken().trim() + File.separator;
-		
-		return str;
-	}
-	
+
 	@Override
 	public void treeWillExpand(TreeExpansionEvent event) throws ExpandVetoException {
-		// TODO Auto-generated method stub
-		System.out.println("getPath: "+event.getPath().toString());
-		if(event.getPath().toString().equals("[]"))
-			return;
-		String parentPath = getPos(event.getPath());
-		System.out.println("parent: "+parentPath);
-		if(parentPath == null || parentPath.trim().length() == 0)
+		/**
+		 * Tree가 확장될 때 불리는 리스너로 파일경로로부터 자식 디렉토리를 노드에 추가한다.
+		 * @param (event) event.getPath()로부터 파일경로를 추출하여 노드 확장에 사용.
+		 */
+		setSelectionPath(event.getPath());
+		String path = getPath(event.getPath());
+		if(path.trim().length() == 0)
 			return;
 		
-		File parentFile = new File(parentPath);
+		File parentFile = new File(path); 
 		File[] childrenFiles = parentFile.listFiles();
-		
+
 		DefaultMutableTreeNode lastNode = (DefaultMutableTreeNode) event.getPath().getLastPathComponent();
 		lastNode.removeAllChildren();
-		for(int i=0; i<childrenFiles.length; i++){
-			if(childrenFiles[i].isDirectory())
-				lastNode.add(new DefaultMutableTreeNode(childrenFiles[i].getName()));
+		int i = 0;
+		if (childrenFiles != null && childrenFiles.length != 0) { // 부모노드에 자식노드(디렉토리) 추가 
+			int dirCount = 0;
+			for (i = 0; i < childrenFiles.length; i++)
+				if (childrenFiles[i].isDirectory()) {
+					DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(childrenFiles[i].getName());
+					childNode.add(new DefaultMutableTreeNode(EMPTYTAG));
+					lastNode.add(childNode);
+					dirCount++;
+				}
+			
+			if(dirCount == 0) // 폴더에 파일만 존재 할 때 
+				lastNode.add(new DefaultMutableTreeNode(EMPTYTAG));
+		}else{ // 빈 폴더 일 때 
+			lastNode.add(new DefaultMutableTreeNode(EMPTYTAG));
 		}
 	}
 
 	@Override
 	public void treeWillCollapse(TreeExpansionEvent event) throws ExpandVetoException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void valueChanged(TreeSelectionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
